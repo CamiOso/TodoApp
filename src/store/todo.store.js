8e24a4e4d251e8d6a567fb2d8a85277b30d0bf74
Cristian Camilo Osorio Granada <crcosoriogr@unal.edu.co>
2024-07-15 08:16:45 -0500

    Se crea la funciona toggleTodo-para cambiar estado

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,95 +1,103 @@
-import {Todo} from '../todos/models/todo.model.js'
+import { Todo } from "../todos/models/todo.model.js";
 
+const Filters = {
+  All: "all",
+  Completed: "Completed",
+  Pending: "Pending",
+};
+const state = {
+  todos: [
+    new Todo("Piedra del alma"),
+    new Todo("Piedra del tiempo"),
+    new Todo("Piedra del infinito"),
+    new Todo("Piedra del poder"),
 
-const Filters={
-    All: 'all',
-    Completed:'Completed',
-    Pending:'Pending'
-}
-const state={
-    todos: [
-        new Todo('Piedra del alma'),
-        new Todo('Piedra del tiempo'),
-        new Todo('Piedra del infinito'),
-    ],
-    filter: Filters.All
-}
+  ],
+  filter: Filters.All,
+};
 
+const initStore = () => {
+  console.log(state);
+  console.log("InitStore");
+};
 
-const initStore =()=>{
-    console.log(state);
-    console.log('InitStore');
-}
+const loadStore = () => {
+  throw new Error("Not implemented yet");
+};
 
+const getTodos = (filter = Filters.All) => {
+  switch (filter) {
+    case Filters.All:
+      return [...state.todos];
+    case Filters.Completed:
+      return state.todos.filter(todo=> todo.done);
+    case Filters.Pending:
+      return state.todos.filter(todo => !todo.done);
 
-const loadStore=()=>{
-    throw new Error('Not implemented yet');
-}
+    default:
+        throw new Error(`Option ${filter} is not valid.`);
 
+  }
+};
 
+/**
+ *
+ * @param {String} description
+ */
 
+const addTodo = (description) => {
+    if(!description)  throw new Error('Description is required');
+    state.todos.push(new Todo(description));
+  
+};
 
-const getTodos=(filter=Filters.All)=>{
-    switch(filter){
-        case Filters.All:
-            return [...state.todos];
-        case Filters.Completed:
-            return state.todos.filter(todo=>todo.done);
-            case Filters.Pending:
-                return state.todos.filter(todo=>!todo.done);
-        
+/**
+ *
+ * @param {String} todoId
+ */
+const toggleTodo = (todoId) => {
+  state.Todo=state.todos.map(todo=>{
+    if(todo.id===todoId){
+      todo.done=!todo.done;
 
     }
+    return todo;
 
+  })
+ 
+  
+};
 
-}
+const deleteTodo = (todoId) => {
+    state.todos=state.todos.filter(todo=>todo.id!==todoId);
+};
 
-/**
+const deleteCompleted = () => {
+    state.todos=state.todos.filter(todo=>todo.done);
+};
+
+/***
  * 
- * @param {String} descripcion 
+ * @param {Filters} newFilter
+ 
  */
 
-const addTodo=(descripcion)=>{
+const setFilter = (newFilter = Filters.All) => {
+ state.filter=newFilter;
+};
 
-throw new Error('Not implemented yet');
-}
-
-
-/**
- * 
- * @param {String} todoId 
- */
-const toggleTodo=(todoId)=>{
-    throw new Error('Not implemented yet');
-
-}
-
-
-const deleteTodo=(todoId)=>{
-    throw new Error('Not implemented yet');
-
-}
-
-const deleteCompleted=()=>{
-    throw new Error('Not implemented yet');
-
-}
-
-const setFilter=(newFilter=Filters.All)=>{
-    throw new Error('Not implemented yet');
-}
-
-const getCurrentFilter=()=>{
-    throw new Error('Not implemented yet');
-}
+const getCurrentFilter = () => {
+ return state.filter;
+};
 
 export default {
-    initStore,
-    getTodos,
-    loadStore,
-    toggleTodo,
-    deleteTodo,
-    deleteCompleted,
-    setFilter,
-    getCurrentFilter,
-}
+    addTodo,
+  initStore,
+  getTodos,
+  loadStore,
+  toggleTodo,
+  deleteTodo,
+  deleteCompleted,
+  setFilter,
+  getCurrentFilter,
+};
